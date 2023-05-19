54bc6671c23147104b320bf4428c522cb6b2bfb9
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-19 13:20:54 +0300

    created auth controller

---
+++ laravel-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255|min:3',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->assignRole('user');
+
+        $response = [
+            'user' => $user,
+            'token' => $user->createToken($user->email)->plainTextToken
+        ];
+
+        return response()->json([
+            'message' => 'Account created successfully',
+            'data' => $response
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'string|email|max:255',
+            'password' => 'string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()], Response::HTTP_BAD_REQUEST);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $response = [
+                'user' => $user,
+                'token' => $user->createToken($user->email)->plainTextToken
+            ];
+            return response()->json([
+                'message' => 'Logged in successfully',
+                'data' => $response
+            ], Response::HTTP_OK);
+        } else {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ], Response::HTTP_OK);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json([
+            'message' => 'User retrieved successfully',
+            'data' => $request->user()
+        ], Response::HTTP_OK);
+    }
+}
