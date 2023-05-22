72830136150a5906aa726fdeca76fce31412198a
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-22 17:31:53 +0300

    frontend - read user from context

--- react-front/src/Router/PrivateRoutes.jsx
+++ react-front/src/Router/PrivateRoutes.jsx
@@ -1,8 +1,10 @@
+import { useContext } from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 const PrivateRoutes = () => {
-  const auth = { token: true }
-  return auth.token ? <Outlet /> : <Navigate replace to='/auth' />
+  const { user } = useContext(UserContext)
+  return user?.token ? <Outlet /> : <Navigate replace to='/auth' />
 }
 
 export default PrivateRoutes
