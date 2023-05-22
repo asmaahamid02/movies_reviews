a32cc0331d47984058bf2531cae533540c5022e0
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-22 19:34:08 +0300

    frontend - added react-query and user provider wrappers

--- react-front/src/main.jsx
+++ react-front/src/main.jsx
@@ -11,6 +11,12 @@ import '@fontsource/roboto/700.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { UserProvider } from './context/UserContext.jsx'
+
+const queryClient = new QueryClient()
+
 const theme = createTheme({
   palette: {
     mode: 'light',
@@ -28,7 +34,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            <App />
+          </UserProvider>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
