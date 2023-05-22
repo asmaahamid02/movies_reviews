7b22e44f8b3edfb555099040430f3281ddfeea25
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-22 17:32:42 +0300

    frontend - redirected user to home if logged

--- react-front/src/Router/Routes.jsx
+++ react-front/src/Router/Routes.jsx
@@ -7,21 +7,30 @@ import Movies from '../components/admin/Movies'
 import Home from '../pages/Home'
 import PrivateRoutes from './PrivateRoutes'
 import NotFoundPage from '../pages/NotFoundPage'
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
 
-const MainRoutes = () => (
-  <Routes>
-    <Route path='/auth' element={<Auth />} />
-    <Route element={<PrivateRoutes />}>
-      <Route path='/' element={<Home />}>
-        <Route path='/' element={<Navigate replace to='dashboard' />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/movies' element={<Movies />} />
+const MainRoutes = () => {
+  const { user } = useContext(UserContext)
+  return (
+    <Routes>
+      <Route
+        path='/auth'
+        exact
+        element={user?.token ? <Navigate replace to='/' /> : <Auth />}
+      />
+      <Route element={<PrivateRoutes />}>
+        <Route path='/' element={<Home />}>
+          <Route path='/' element={<Navigate replace to='dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/movies' element={<Movies />} />
+        </Route>
       </Route>
-    </Route>
 
-    <Route path='*' element={<NotFoundPage />} />
-  </Routes>
-)
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  )
+}
 
 export default MainRoutes
