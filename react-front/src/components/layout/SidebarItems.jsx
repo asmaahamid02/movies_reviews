806cad8f1cd6ca70c1dc492dc2e683d3ca95bde6
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-23 17:45:55 +0300

    frontend - modified sidebar items

--- react-front/src/components/layout/SidebarItems.jsx
+++ react-front/src/components/layout/SidebarItems.jsx
@@ -1,17 +1,14 @@
-import * as React from 'react'
-import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import PeopleIcon from '@mui/icons-material/People'
-import BarChartIcon from '@mui/icons-material/BarChart'
-import LayersIcon from '@mui/icons-material/Layers'
+import MovieIcon from '@mui/icons-material/Movie'
 import { ListItem } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 export const mainListItems = (
-  <React.Fragment>
-    <ListItem component={Link} to='/dashboard'>
+  <>
+    <ListItem component={Link} to='/home'>
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
@@ -22,26 +19,13 @@ export const mainListItems = (
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
-
       <ListItemText primary='Users' />
     </ListItem>
-    <ListItemButton>
+    <ListItem component={Link} to='/movies'>
       <ListItemIcon>
-        <PeopleIcon />
+        <MovieIcon />
       </ListItemIcon>
-      <ListItemText primary='Customers' />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary='Reports' />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary='Integrations' />
-    </ListItemButton>
-  </React.Fragment>
+      <ListItemText primary='Movies' />
+    </ListItem>
+  </>
 )
