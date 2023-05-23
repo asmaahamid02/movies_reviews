3ce46cb8db23b8e7a882852cf202e2baac47f5d7
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-23 12:52:05 +0300

    added role based routes

--- react-front/src/Router/PrivateRoutes.jsx
+++ react-front/src/Router/PrivateRoutes.jsx
@@ -1,9 +1,22 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate, useLocation } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import PropTypes from 'prop-types'
 
-const PrivateRoutes = () => {
+const PrivateRoutes = ({ allowedRoles }) => {
   const { user } = useAuth()
-  return user?.token ? <Outlet /> : <Navigate replace to='/auth' />
+  const location = useLocation()
+
+  return user?.roles.find((role) => allowedRoles?.includes(role)) ? (
+    <Outlet />
+  ) : user?.token ? (
+    <Navigate replace to='/unauthorized' state={{ from: location }} />
+  ) : (
+    <Navigate replace to='/auth' state={{ from: location }} />
+  )
+}
+
+PrivateRoutes.propTypes = {
+  allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default PrivateRoutes
--- react-front/src/Router/Routes.jsx
+++ react-front/src/Router/Routes.jsx
@@ -1,31 +1,47 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Auth from '../pages/Auth'
-import Dashboard from '../components/admin/Dashboard'
-import Users from '../components/admin/Users'
-import Movies from '../components/admin/Movies'
+import UsersList from '../components/admin/UsersList'
+import MoviesList from '../components/admin/MoviesList'
 import Home from '../pages/Home'
 import PrivateRoutes from './PrivateRoutes'
-import NotFoundPage from '../pages/NotFoundPage'
-import useAuth from '../hooks/useAuth'
+import NotFoundPage from '../pages/errors/NotFoundPage'
+import NotAuthorized from '../pages/errors/NotAuthorized'
+import Dashboard from '../components/Dashboard'
+import SingleMovie from '../components/user/SingleMovie'
 
 const MainRoutes = () => {
-  const { user } = useAuth()
   return (
     <Routes>
+      {/* public routes */}
+      <Route path='/auth' exact element={<Auth />} />
+
+      {/* admin and user routes */}
       <Route
-        path='/auth'
-        exact
-        element={user?.token ? <Navigate replace to='/' /> : <Auth />}
-      />
-      <Route element={<PrivateRoutes />}>
+        path='/'
+        element={<PrivateRoutes allowedRoles={['admin', 'user']} />}
+      >
+        <Route path='/' element={<Navigate replace to='home' />} />
+        <Route path='/home' element={<Dashboard />} />
+      </Route>
+
+      {/* admin routes */}
+      <Route element={<PrivateRoutes allowedRoles={['admin']} />}>
         <Route path='/' element={<Home />}>
-          <Route path='/' element={<Navigate replace to='dashboard' />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='/movies' element={<Movies />} />
+          <Route path='/users' element={<UsersList />} />
+          <Route path='/movies' element={<MoviesList />} />
         </Route>
       </Route>
+
+      {/* user routes */}
+      <Route element={<PrivateRoutes allowedRoles={['user']} />}>
+        <Route path='/' element={<Home />}>
+          <Route path='/movie/:movieId' element={<SingleMovie />} />
+        </Route>
+      </Route>
+
+      {/* error routes */}
+      <Route path='/unauthorized' element={<NotAuthorized />} />
 
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
