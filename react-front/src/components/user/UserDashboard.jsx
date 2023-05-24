6dae92cd6b0c8affd792d0c9928bc7cee7266bf3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-24 17:56:09 +0300

    frontend - displayed movies for user

--- react-front/src/components/user/UserDashboard.jsx
+++ react-front/src/components/user/UserDashboard.jsx
@@ -1,8 +1,60 @@
+import { Alert, Backdrop, Button, CircularProgress, Grid } from '@mui/material'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { getMovies } from '../../services/movie.service'
+import MovieCard from './MovieCard'
+
+const pageSize = 15
 const UserDashboard = () => {
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['movies', 'infinite'],
+    getNextPageParam: (prevData) => prevData?.data?.nextPage,
+    queryFn: ({ pageParam = 1 }) => getMovies(pageParam, pageSize),
+  })
+
+  if (isLoading) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color='inherit' />
+      </Backdrop>
+    )
+  }
+
+  if (isError) {
+    return <Alert severity='error'>{error.response.data.message}</Alert>
+  }
+
+  if (data.pages[0].data.movies.length === 0) {
+    return <Alert severity='info'>No movies found</Alert>
+  }
+
   return (
-    <div>
-      <h1>User Dashboard</h1>
-    </div>
+    <>
+      <Grid container spacing={2}>
+        {data.pages
+          .flatMap((page) => page.data.movies)
+          .map((movie) => (
+            <Grid item key={movie.id}>
+              <MovieCard movie={movie} />
+            </Grid>
+          ))}
+      </Grid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? 'Loading more...' : 'Load More'}
+        </Button>
+      )}
+    </>
   )
 }
 
