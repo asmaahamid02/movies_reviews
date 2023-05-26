8410c4683cec6b1aabf3b4a6e0ed2a5d3a3a1d54
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-26 19:03:40 +0300

    backend - modified store api

--- laravel-backend/app/Http/Controllers/ActorController.php
+++ laravel-backend/app/Http/Controllers/ActorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Actor;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class ActorController extends Controller
@@ -31,31 +32,81 @@ class ActorController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255|min:3',
-            'image_url' => 'required|string|max:255',
-            'birthday' => 'required|date',
-            'biography' => 'required|string',
-            'birthplace' => 'required|string',
-            'nationality' => 'required|string',
-            'gender' => 'required|string|in:male,female,other',
+            //actors is an array of objects
+            'actors' => 'required|array|min:1',
+            'actors.*' => 'required',
+            'actors.*.name' => 'required|string|max:255|min:3',
+            'actors.*.image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'actors.*.birthday' => 'required|date|before:today',
+            'actors.*.biography' => 'required|string',
+            'actors.*.birthplace' => 'required|string',
+            'actors.*.nationality' => 'required|string',
+            'actors.*.gender' => 'required|string|in:male,female,other',
+            'movie_id' => 'nullable|integer|exists:movies,id',
         ]);
 
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first()], Response::HTTP_BAD_REQUEST);
         }
 
-        $actor = new Actor();
-        $actor->fill($request->only(['name', 'image_url', 'birthday', 'biography', 'birthplace', 'nationality', 'gender']));
-        $saved = $actor->save();
+        $actors = $request->actors;
 
-        if (!$saved) {
-            return response()->json(['message' => 'Cannot save actor'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        try {
+            DB::beginTransaction();
+            foreach ($actors as $actor) {
+
+
+                $imageName = time() . '.' . $actor['image']->extension();
+                $path = 'assets/images/actors/' . $imageName;
+                $actor['image']->move(public_path('assets/images/actors'), $imageName);
+                $saved = Actor::create([
+                    'name' => $actor['name'],
+                    'birthday' => $actor['birthday'],
+                    'biography' => $actor['biography'],
+                    'birthplace' => $actor['birthplace'],
+                    'nationality' => $actor['nationality'],
+                    'gender' => $actor['gender'],
+                    'image_url' => URL('/') . '/' . $path,
+                ]);
+
+                if ($request->movie_id) {
+                    $saved->movies()->attach($request->movie_id);
+                }
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage() . ' at ' . $e->getLine()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
-        return response()->json([
-            'message' => 'Actor created successfully',
-            'data' => $actor
-        ], Response::HTTP_CREATED);
+
+        return response()->json(['message' => 'Actors saved successfully'], Response::HTTP_OK);
+        // $validator = Validator::make($request->all(), [
+        //     'name' => 'required|string|max:255|min:3',
+        //     'image_url' => 'required|string|max:255',
+        //     'birthday' => 'required|date',
+        //     'biography' => 'required|string',
+        //     'birthplace' => 'required|string',
+        //     'nationality' => 'required|string',
+        //     'gender' => 'required|string|in:male,female,other',
+        // ]);
+
+        // if ($validator->fails()) {
+        //     return response()->json(['message' => $validator->errors()->first()], Response::HTTP_BAD_REQUEST);
+        // }
+
+        // $actor = new Actor();
+        // $actor->fill($request->only(['name', 'image_url', 'birthday', 'biography', 'birthplace', 'nationality', 'gender']));
+        // $saved = $actor->save();
+
+        // if (!$saved) {
+        //     return response()->json(['message' => 'Cannot save actor'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // }
+
+        // return response()->json([
+        //     'message' => 'Actor created successfully',
+        //     'data' => $actor
+        // ], Response::HTTP_CREATED);
     }
 
     /**
