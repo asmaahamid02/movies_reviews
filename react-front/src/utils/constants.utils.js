5f7b213bb18a52c176187a07c2816090a6d17080
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-05-26 22:10:58 +0300

    frontend - added style object

--- react-front/src/utils/constants.utils.js
+++ react-front/src/utils/constants.utils.js
@@ -18,3 +18,17 @@ export const API = {
   // Directors
   directors: `${BASEURL}/directors`,
 }
+
+export const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '75%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  maxHeight: '90vh',
+  overflowY: 'auto',
+}
